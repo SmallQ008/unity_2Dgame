8a639dd44bea0d0bc1bda9ff31bc096510f03a2c
SmallQ008 <71476570+SmallQ008@users.noreply.github.com>
2021-03-12 11:59:41 +0800

    QQ 2D

--- unity_HWH_2D_QQ/Assets/Player.cs
+++ unity_HWH_2D_QQ/Assets/Player.cs
@@ -3,5 +3,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  
+    [Header("等級")]
+   public int lv=1;
+    [Header ("移動速度"), Range (0 , 300)]
+    public float speed = 10.5f;
+    public bool isDead = false;
+    [Tooltip("這是角色的名稱")]
+    public string cName = "貓咪";
+
 }
