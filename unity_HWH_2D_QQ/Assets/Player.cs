0c5542d1d4935d2e9f054d46e6050275c47977cc
SmallQ008 <71476570+SmallQ008@users.noreply.github.com>
2021-03-19 10:08:21 +0800

    0319QQ

--- unity_HWH_2D_QQ/Assets/Player.cs
+++ unity_HWH_2D_QQ/Assets/Player.cs
@@ -10,5 +10,38 @@ public class Player : MonoBehaviour
     public bool isDead = false;
     [Tooltip("這是角色的名稱")]
     public string cName = "貓咪";
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    
+    private void Move()
+    {
+        print("移動");
+        float h = joystick.Horizontal;
+        print("水平:" + h);
+        float v = joystick.Vertical;
+        print("垂直:" + v);
+    }
+    private void Attack()
+    {
+
+    }
+    private void Hit()
+    {
+
+    }
+    private void Dead()
+    {
+
+    }
+    private void Start()
+    {
+        Move();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
 
 }
