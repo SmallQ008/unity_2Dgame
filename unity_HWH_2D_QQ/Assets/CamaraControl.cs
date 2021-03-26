99fb49f35d9ed67c7c21c484c82cb7bac7f0845b
unknown <71476570+SmallQ008@users.noreply.github.com>
2021-03-26 09:56:32 +0800

    0326

--- unity_HWH_2D_QQ/Assets/CamaraControl.cs
+++ unity_HWH_2D_QQ/Assets/CamaraControl.cs
@@ -10,4 +10,40 @@ public class CamaraControl : MonoBehaviour
     public Vector2 limitY = new Vector2(-5, 5);
     [Header("左右邊界")]
     public Vector2 limitX = new Vector2(-5, 5);
+
+    [Header("玩家")]
+    public Transform player;
+    private void Update()
+    {
+        Track();
+    }
+    
+    private void Track()
+    {
+        Vector3 posCam = transform.position;
+        Vector3 posPla = player.position;
+
+        posCam = Vector3.Lerp(posCam, posPla, 0.5f *speed* Time.deltaTime);
+        posCam.z = -10;
+        posCam.x = Mathf.Clamp(posCam.x,limitX.x,limitX.y);
+        posCam.y = Mathf.Clamp(posCam.y,limitY.x,limitY.y);
+
+        transform.position = posCam;
+
+        //print(posCam);
+    }
+
+    //public float a = 0;
+    //public float b = 100;
+
+    //public Vector3 v3a = new Vector3(0, 0, 0);
+    //public Vector3 v3b = new Vector3(100, 100, 100);
+
+
+    //private void Update()
+    //{
+
+        //a = Mathf.Lerp(a, b, 0.5f);
+        //v3a = Vector3.Lerp(v3a, v3b, 0.5f);
+    //}
 }
